777bb4d5ba0ec1a1fdd02af84e4d5ca9cdb49753
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-01-23 22:27:23 -0500

    Control code + helpers

---
+++ brownie/scripts/helpers.py
@@ -0,0 +1,18 @@
+from brownie import network, accounts, config, Contract
+from brownie.network import contract
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development","ganache-local"]
+FORKED = ["mainnet-fork","mainnet-fork-dev"]
+
+def get_account(index = 0, id = None): # Automaticaly gets a good account
+    if id != None:
+        return accounts.load(id)
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED:
+        return accounts[index]
+    return accounts.load("test1")
+
+def smart_get_account(index):
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[index]
+    return accounts.load("test"+str(index+1))
+    
---
+++ brownie/scripts/runAstroSwap.py
@@ -0,0 +1,74 @@
+from scripts.helpers import smart_get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from brownie import network, accounts, config, AstroSwapExchange, MockERC20
+
+fee = 400
+
+tokenCounter = 0
+tokens = []
+
+def deploy_erc20():
+    global tokenCounter
+    account = smart_get_account(1)
+    print("account:", account)
+    erc20 = MockERC20.deploy(
+        str(tokenCounter),
+        {'from': account},
+        publish_source = config["networks"][network.show_active()].get("verify", False)
+        )
+    print("ERC20@", erc20)
+    tokenCounter += 1
+    tokens.append(erc20)
+    return erc20
+
+def deploy_exchage_contract(tokenAddress):
+    account = smart_get_account(1)
+    print("account:", account)
+    exchange = AstroSwapExchange.deploy(
+        tokenAddress,
+        fee,
+        {'from': account},
+        publish_source = config["networks"][network.show_active()].get("verify", False)
+        )
+    print("AstroSwapExchage@", exchange)
+    return exchange
+
+def get_exchange_info():
+    exchange = AstroSwapExchange[-1]
+    print("Address:", exchange.address, "Token:", exchange.token() ,"Fee:", exchange.feeAmmount(), "ETH/ERC20:", exchange.ethPool(), "/", exchange.tokenPool(), "Invariant:", exchange.invariant())
+
+def seed_invest(eth, token):
+    exchange = AstroSwapExchange[-1]
+    account = smart_get_account(1)
+    # Allow the contract to transfer 100 of our tokens
+    approveTx = tokens[0].approve(exchange.address, token, {'from': account})
+    approveTx.wait(1)
+    print("account:", account)
+    investTx = exchange.seedInvest(token, {'from': account, 'value': eth})
+    investTx.wait(1)
+    print("Seeded invest", investTx.events)
+
+def buy_tokens(eth, min_tokens=0):
+    exchange = AstroSwapExchange[-1]
+    account = smart_get_account(1)
+    buyTx = exchange.ethToToken(account.address, min_tokens, {'from': account, 'value': eth})
+    buyTx.wait(1)
+    print("Bought tokens", buyTx.events)
+
+def buy_eth(token, min_eth=0):
+    exchange = AstroSwapExchange[-1]
+    account = smart_get_account(1)
+    # Need to authorise the transfer of tokens
+    approveTx = tokens[0].approve(exchange.address, token, {'from': account})
+    approveTx.wait(1)
+    buyTx = exchange.tokenToEth(account.address,token, min_eth, {'from': account})
+    buyTx.wait(1)
+    print("Bought eth", buyTx.events)
+
+def main():
+    deploy_erc20()
+    deploy_exchage_contract(tokens[0].address)
+    get_exchange_info()
+    seed_invest(1000, 50000)
+    get_exchange_info()
+    buy_tokens(10000)
+    get_exchange_info()
