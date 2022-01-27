0adeed6ec861a946673921d4b5e55d09b87e07e2
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-01-26 21:21:18 -0500

    Controller code for the factory and it's usescases

--- brownie/scripts/runAstroFactory.py
+++ brownie/scripts/runAstroFactory.py
@@ -1,6 +1,7 @@
-from scripts.helpers import smart_get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
-from brownie import network, accounts, config, AstroSwapExchange, AstroSwapFactory, MockERC20
-from scripts.runAstroSwap import deploy_erc20
+from scripts.helpers import smart_get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS, approve_transfer
+from brownie import network, accounts, config, AstroSwapExchange, AstroSwapFactory
+from brownie.network.contract import Contract
+from scripts.runAstroSwap import deploy_erc20, seed_invest, exchange_from_address
 
 fee = 400
 
@@ -29,10 +30,37 @@ def get_factory_info(factory = None):
     if factory == None: factory = AstroSwapFactory[-1]
     print("Address:", factory.address, "Exchange count:", factory.exchangeCount())
 
+def tokenToToken(token1, token2, amount, factoryAddress):
+    account = smart_get_account(1)
+    print("account:", account)
+    token1Exchange = factoryAddress.convertTokenToExchange(token1)
+    exchange1 = exchange_from_address(token1Exchange)
+    token1ToToken2Tx = exchange1.tokenToToken(account, token2, amount, 0, {'from': account})
+    token1ToToken2Tx.wait(1)
+    print("TTT Events:", token1ToToken2Tx.events)
+
+def tokenToTokenQuote(token1, token2, amount, factoryAddress):
+    account = smart_get_account(1)
+    print("account:", account)
+    token1Exchange = factoryAddress.convertTokenToExchange(token1)
+    print("token1Exchange:", token1Exchange)
+    exchange1 = exchange_from_address(token1Exchange)
+    print("exchange1:", exchange1)
+    quote = exchange1.getTokenToTokenQuote(amount, token2)
+    print("TTT Quote:", quote)
+    return quote
+
 def main():
     erc20 = deploy_erc20()
     factory = deploy_factory_contract()
     tokenExchangeAddress = deploy_new_exchange(factory, erc20)
-    get_factory_info()
-    print("TTE:", factory.convertTokenToExchange(erc20.address))
-    print("ETT:", factory.convertExchangeToToken(tokenExchangeAddress))
+    erc20_2 = deploy_erc20()
+    tokenExchangeAddress_2 = deploy_new_exchange(factory, erc20_2)
+    approve_transfer(erc20.address, tokenExchangeAddress, smart_get_account(1), 100)
+    approve_transfer(erc20_2.address, tokenExchangeAddress_2, smart_get_account(1), 100)
+    seed_invest(100, 100, exchange=exchange_from_address(tokenExchangeAddress))
+    seed_invest(100, 100, exchange=exchange_from_address(tokenExchangeAddress_2))
+    print("Seed investments complete")
+    approve_transfer(erc20, tokenExchangeAddress, smart_get_account(1), 20)
+    tokenToTokenQuote(erc20, erc20_2, 20, factory)
+    tokenToToken(erc20, erc20_2, 20, factory)
